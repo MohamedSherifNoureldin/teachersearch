a27dc7d12d6ffb9ca75abcbebb45147efca8da07
Mohamed sherif noureldin <mohamedsherifhazem@gmail.com>
2017-09-15 14:28:59 +0200

    added the spelling solution and useranme

--- teachers.sql
+++ teachers.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Sep 11, 2017 at 07:30 PM
+-- Generation Time: Sep 15, 2017 at 02:27 PM
 -- Server version: 10.1.21-MariaDB
 -- PHP Version: 7.1.1
 
@@ -32,17 +32,19 @@ CREATE TABLE `teachers` (
   `center` varchar(255) NOT NULL,
   `subject` varchar(255) NOT NULL,
   `location` varchar(255) NOT NULL,
-  `phone` varchar(20) NOT NULL
+  `phone` varchar(20) NOT NULL,
+  `username` varchar(255) NOT NULL,
+  `other_names` text
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `teachers`
 --
 
-INSERT INTO `teachers` (`id`, `name`, `center`, `subject`, `location`, `phone`) VALUES
-(1, 'Khaled rateb', 'Friends', 'Bio OL', 'Nasr City', '01066992782'),
-(2, 'Heba Fathi', 'Home', 'English OL', 'Sheraton', '01066992782'),
-(3, 'Kareem Abdelsamd', 'Home', 'Bio OL', 'Nasr City', '01066777772');
+INSERT INTO `teachers` (`id`, `name`, `center`, `subject`, `location`, `phone`, `username`, `other_names`) VALUES
+(1, 'Khaled rateb', 'Friends', 'Math OL', 'Nasr City', '01066992782', 'khaledrateb', 'khled, khalid'),
+(2, 'Heba Fathi', 'Home', 'English OL', 'Sheraton', '01066992782', 'hebafathi', 'hba, hiba, hebe'),
+(3, 'Kareem Abdelsamd', 'Home', 'Bio OL', 'Nasr City', '01066777772', 'kareemabdelsamed', 'karim, karem, kreem, krim');
 
 --
 -- Indexes for dumped tables
